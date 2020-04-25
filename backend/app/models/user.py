2066f067efdc475c3bcaee6c141d0077d6aa225b
Oleh Kyba <vestigor0@gmail.com>
2020-04-25 12:30:56 +0300

    Add public_id and status columns.

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,11 +1,19 @@
+from enum import Enum
+from uuid import uuid4
 from app.extentions import db
+from utils.sa_guid import GUID
+
+
+class UserStatus(Enum):
+
+    active = 'active'
+    email_is_not_verified = 'email is not verified'
 
 
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    surname = db.Column(db.String(50), nullable=False)
-    patronymic = db.Column(db.String(50), nullable=True)
+    public_id = db.Column(GUID, default=uuid4, unique=True)
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
+    status = db.Column(db.Enum(UserStatus), default=UserStatus.email_is_not_verified)
