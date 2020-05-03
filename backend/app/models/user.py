46dd17249010dd1e53f1af13f24a789d1af59c6a
Oleh Kyba <vestigor0@gmail.com>
2020-05-03 14:13:01 +0300

    Add password hashing

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from uuid import uuid4
+from werkzeug.security import generate_password_hash, check_password_hash
 from app.extentions import db
 from utils.sa_guid import GUID
 
@@ -7,7 +8,7 @@ from utils.sa_guid import GUID
 class UserStatus(Enum):
 
     active = 'active'
-    email_is_not_verified = 'email_is_not_verified'
+    email_is_not_verified = '/'
 
 
 class User(db.Model):
@@ -17,3 +18,9 @@ class User(db.Model):
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
     status = db.Column(db.Enum(UserStatus), nullable=False, default=UserStatus.email_is_not_verified)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def is_correct_password(self, password):
+        return check_password_hash(self.password, password)
